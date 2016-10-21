e03d07ea1b30c7e93288ac520bf8d7e1589ebe77
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-10-21 14:54:27 +1300

    Simple Solidus 1.3 compatibility
    A more fundamental rewrite would be in order here, but this should suffice in the short term to get things working.

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,32 +1,26 @@
 Spree::LineItem.class_eval do
-  # Sets this line item's price, cost price, and currency from this line
-  # item's variant if they are nil and a variant is present.
   # Overridden to check special prices for particular users or roles
-  def copy_price
-    return nil unless variant
-    self.price = variant.user_price(order.user) if price.nil?
-    self.cost_price = variant.cost_price if cost_price.nil?
-    self.currency = variant.currency if currency.nil?
+  def set_pricing_attributes
+    return handle_copy_price_override if respond_to?(:copy_price)
+
+    self.currency ||= order.currency
+    self.cost_price ||= variant.cost_price
+    self.money_price = variant.user_price_for(pricing_options, order.user) if price.nil?
+    true
   end
 
-  # Sets the options on the line item according to the order's currency or
-  # one passed in.
-  # @param options [Hash] options for this line item
   # Overridden to use user prices if available for the order's user
   def options=(options = {})
     return unless options.present?
     # We will be deleting from the hash, so leave the caller's copy intact
     opts = options.dup
-    currency = opts.delete(:currency) || order.try(:currency)
     user = opts.delete(:user)
-    if currency
-      self.currency = currency
-      self.price    = variant.user_price_for(pricing_options, user).amount +
-                      variant.price_modifier_amount_in(currency, opts)
-    else
-      self.price    = variant.user_price(user) +
-                      variant.price_modifier_amount(opts)
-    end
+
     assign_attributes opts
+
+    # There's no need to call a pricer if we'll set the price directly.
+    unless opts.key?(:price) || opts.key?('price')
+      self.money_price = variant.user_price_for(pricing_options, user)
+    end
   end
 end
