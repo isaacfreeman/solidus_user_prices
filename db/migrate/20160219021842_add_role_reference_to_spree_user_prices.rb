2258161203220297e77c01c77299166fb9fd0655
Peter Berkenbosch <peter@pero-ict.nl>
2017-07-03 09:43:05 +0200

    Update migrations to inherit from AR::Migration[4.2]
    To make this work in Rails > 5 we have to specify the migration version
    that the migration was written for. This commit make the migrations
    suitable from 4.2, so it will be working for older versions of Solidus
    as well.

--- db/migrate/20160219021842_add_role_reference_to_spree_user_prices.rb
+++ db/migrate/20160219021842_add_role_reference_to_spree_user_prices.rb
@@ -1,4 +1,4 @@
-class AddRoleReferenceToSpreeUserPrices < ActiveRecord::Migration
+class AddRoleReferenceToSpreeUserPrices < ActiveRecord::Migration[4.2]
   def change
     add_reference :spree_user_prices, :role, index: true
   end
